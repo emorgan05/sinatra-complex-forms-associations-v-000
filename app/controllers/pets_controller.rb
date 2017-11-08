6fe3678d55e9f8a1fbda30ef7346dd029f8c9f3c
Emily Morgan <emorgan05@gmail.com>
2017-11-08 14:48:59 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,6 +30,15 @@ class PetsController < ApplicationController
     erb :'/pets/edit'
   end
 
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect to "pets/#{@pet.id}"
+  end
+  
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
